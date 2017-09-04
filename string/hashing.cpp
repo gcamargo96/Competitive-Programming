8861c7268f41601f22ee6490bc147c3d1726a0b5
Gabriel Camargo <gacamargo1.000@gmail.com>
2017-09-04 20:20:04 -0300

    Update hashing.cpp

--- string/hashing.cpp
+++ string/hashing.cpp
@@ -1,26 +1,49 @@
-#include <cstdio>
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 0X3f3f3f3f
+#define M 1000000007
+#define For(i,a,b) for(int (i)=(a);(i) < (b); ++(i))
+#define mp make_pair
+#define pb push_back 
+#define fi first
+#define se second
+#define endl "\n"
+#define PI acos(-1)
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef pair<int,int> ii;
+typedef complex<double> base;
 
 // another possible base is 7
 #define B 33
-
-using namespace std;
-
-typedef long long ll;
-int a, b, c, d, n;
+string s;
+int a, b, c, d, k, n;
 ll mod[2] = {1000000007, 1000000009};
-ll h[2][10000000];
-ll pot[2][10000000];
+ll h[2][10000000], hr[2][10000000];
+ll pot[2][10000000], potr[2][10000000];
 
-void precalc(string &s){
+void precalc(string& s){
     for(int k = 0; k < 2; k++){
-        h[k][0] = s[0] - 'A' + 1;
+        h[k][0] = s[0]/* - 'A' + 1*/;
         pot[k][0] = 1;
 
         for(int i = 1; i < s.size(); i++){
-            h[k][i] = (h[k][i-1]*B)%mod[k] + (s[i] - 'A' + 1)%mod[k];
+            h[k][i] = ((h[k][i-1]*B)%mod[k] + (s[i]/* - 'A' + 1*/))%mod[k];
             pot[k][i] = (pot[k][i-1]*B)%mod[k];
+        }
+    }
+}
+
+void precalcrev(string& s){
+    int n = s.size();
+    for(int k = 0; k < 2; k++){
+        hr[k][n-1] = s[n-1]/* - 'A' + 1*/;
+        potr[k][n-1] = 1;
+
+        for(int i = n-2; i >= 0; i--){
+            hr[k][i] = ((hr[k][i+1]*B)%mod[k] + (s[i]/* - 'A' + 1*/))%mod[k];
+            potr[k][i] = (potr[k][i+1]*B)%mod[k];
         }
     }
 }
@@ -30,28 +53,7 @@ ll calcHash(int i, int j, int k){
     return (h[k][j] - (h[k][i-1]*pot[k][j-i+1])%mod[k] + mod[k])%mod[k];
 }
 
-int main(void){
-	string s;
-	cin >> s >> n;
-
-	precalc(s);
-
-	ll h1, h2, h3, h4;
-
-	for(int i = 0; i < n; i++){
-		scanf("%d%d%d%d", &a, &b, &c, &d); // verify if two substrings are equals
-        // 0 <= a <= b <= s.size()-1 / 0 <= c <= d <= s.size()-1
-        h1 = calcHash(a, b, 0);
-        h2 = calcHash(a, b, 1);
-        h3 = calcHash(c, d, 0);
-        h4 = calcHash(c, d, 1);
-
-        if(h1 == h3 && h2 == h4){
-            printf("EQUALS\n");
-        }else{
-            printf("NOT EQUALS\n");
-        }
-	}
-
-	return 0;
+ll calcHashRev(int i, int j, int k){
+    if(j == n-1) return hr[k][i];
+    return (hr[k][i] - (hr[k][j+1]*potr[k][n-1-(j-i+1)])%mod[k] + mod[k])%mod[k];
 }
