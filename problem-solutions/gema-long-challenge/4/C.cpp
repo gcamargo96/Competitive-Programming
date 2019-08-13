d2d16a703c00a6aa29bd846539205dbb95a1a1a2
gcamargo96 <gacamargo1.000@gmail.com>
2019-08-13 07:25:09 -0300

    Add treap and segtree beats

---
+++ problem-solutions/gema-long-challenge/4/C.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define For(i,a,b) for(int (i)=(a);(i) < (b); ++(i))
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define PI acos(-1)
+#define fastcin ios_base::sync_with_stdio(false);
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef pair<int,int> ii;
+typedef complex<double> base;
+
+const int N = 100005;
+ll mn[4*N], mx[4*N], clf[4*N], lazy[4*N];
+int a, b;
+int n, m;
+
+void build(int r, int i, int j){
+	if(i == j){
+		mn[r] = mx[r] = i;
+		return;
+	}
+
+	int mid = (i+j)/2;
+	build(2*r, i, mid);
+	build(2*r+1, mid+1, j);
+
+	mn[r] = min(mn[2*r], mn[2*r+1]);
+	mx[r] = max(mx[2*r], mx[2*r+1]);
+}
+
+void prop(int r, int i, int j){
+	if(lazy[r]){
+		clf[r] += ll(j-i+1) * lazy[r];
+
+		if(i != j){
+			lazy[2*r] = lazy[r];
+			mn[2*r] = mx[2*r] = mn[r];
+			
+			lazy[2*r+1] = lazy[r];
+			mn[2*r+1] = mx[2*r+1] = mn[r];
+		}
+
+		lazy[r] = 0;
+	}
+}
+
+void update(int r, int x, int i, int j){
+	prop(r, i, j);
+	if(i > b or j < a) return;
+	if(i >= a and j <= b and mn[r] == mx[r]){
+		lazy[r] = abs(mx[r]-x);
+		mn[r] = mx[r] = x;
+		prop(r, i, j);
+		return;
+	}
+
+	int mid = (i+j)/2;
+	update(2*r, x, i, mid);
+	update(2*r+1, x, mid+1, j);
+
+	clf[r] = clf[2*r] + clf[2*r+1];
+	mn[r] = min(mn[2*r], mn[2*r+1]);
+	mx[r] = max(mx[2*r], mx[2*r+1]);
+}
+
+ll query(int r, int i, int j){
+	prop(r, i, j);
+	if(i > b or j < a) return 0;
+	if(i >= a and j <= b){
+		return clf[r];
+	}
+	
+	// printf("[%d,%d] = %lld\n", i, j, clf[r]);
+
+	int mid = (i+j)/2;
+	ll L = query(2*r, i, mid);
+	ll R = query(2*r+1, mid+1, j);
+	return L+R;
+}
+
+void print(){
+	for(int i = 1; i <= n; i++){
+		a = b = i;
+		printf("%d ", query(1, 1, n));
+	}
+	printf("\n");
+}
+
+int main(void){
+	scanf("%d%d", &n, &m);
+
+	build(1, 1, n);
+
+	// print();
+
+	int tipo, l, r, x;
+	while(m--){
+		scanf("%d", &tipo);
+
+		if(tipo == 1){
+			scanf("%d%d%d", &l, &r, &x);
+			a = l, b = r;
+			update(1, x, 1, n);
+		}
+		else{
+			scanf("%d%d", &l, &r);
+			a = l, b = r;
+			ll ans = query(1, 1, n);
+			printf("%lld\n", ans);
+		}
+		// print();
+	}
+
+	return 0;
+}
---
+++ problem-solutions/practice/treap/TREAP.cpp
@@ -0,0 +1,203 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF 0x3f3f3f3f
+
+struct item {
+	int key, prior, cnt, mx, mn, mindif;
+	item *l, *r;
+
+	item() {}
+
+	item(int x){
+		key = x;
+		prior = rand();
+		cnt = 0;
+		mx = mn = x;
+		mindif = INF;
+	}
+
+	item(int key, int prior) : key(key), prior(prior), l(NULL), r(NULL) {}
+};
+
+typedef item *treap;
+
+int cnt (treap t) {
+    return t ? t->cnt : 0;
+}
+
+int mn(treap t){
+	return t ? t->mn : INF;
+}
+
+int mx(treap t){
+	return t ? t->mx : -INF;
+}
+
+int mindif(treap t){
+	return t ? t->mindif : INF;
+}
+
+void upd (treap t) {
+    if (t){
+        t->cnt = 1 + cnt(t->l) + cnt (t->r);
+        t->mn = min(t->key, mn(t->l));
+        t->mx = max(t->key, mx(t->r));
+        t->mindif = t->cnt == 1 ? INF : min(min(mindif(t->l), mindif(t->r)), min(t->key-mx(t->l), mn(t->r)-t->key));
+    }
+}
+
+void split(treap t, int x, treap& l, treap& r){
+	if(!t){
+		l = r = NULL;
+	}
+	else if(x < t->key){
+		split(t->l, x, l, t->l);
+		r = t;
+	}
+	else{
+		split(t->r, x, t->r, r);
+		l = t;
+	}
+	upd(t);
+}
+
+void split_pos(treap t, int pos, treap& l, treap& r, int acc = 0){
+	if(!t){
+		l = r = NULL;
+	}
+	else if(pos < acc + cnt(t->l) + 1){
+		split_pos(t->l, pos, l, t->l, acc);
+		r = t;
+	}
+	else{
+		split_pos(t->r, pos, t->r, r, acc + cnt(t->l) + 1);
+		l = t;
+	}
+	upd(t);
+}
+
+void insert (treap & t, treap it) {
+    if (!t)
+        t = it;
+    else if(it->key == t->key){
+    	return;
+    }
+    else if (it->prior > t->prior){
+        split (t, it->key, it->l, it->r),  t = it;
+    }
+    else
+        insert (it->key < t->key ? t->l : t->r, it);
+    upd(t);
+}
+
+void merge (treap & t, treap l, treap r) {
+    if (!l || !r)
+        t = l ? l : r;
+    else if (l->prior > r->prior)
+        merge (l->r, l->r, r),  t = l;
+    else
+        merge (r->l, l, r->l),  t = r;
+    upd(t);
+}
+
+void erase (treap & t, int x) {
+    if (t->key == x)
+        merge (t, t->l, t->r);
+    else
+        erase (x < t->key ? t->l : t->r, x);
+    upd(t);
+}
+
+int find_kth(treap t, int k, int acc = 0){
+	if(!t) return -1;
+
+	if(acc + cnt(t->l) + 1 == k){
+		return t->key;
+	}
+	else if(acc + cnt(t->l) + 1 < k){
+		return find_kth(t->r, k, acc + cnt(t->l) + 1);
+	}
+	else{ // acc + cnt(t->l) + 1 > k
+		return find_kth(t->l, k, acc);
+	}
+}
+
+int find_mindif(treap t, int i, int j){
+	if(i == j) return -1;
+
+	treap L = NULL, R = NULL;
+	split_pos(t, i-1, L, t);
+	split_pos(t, j-i+1, t, R);
+
+	if(cnt(t) == 1) return t->key;
+
+	int ans = mindif(t);
+
+	merge(t, L, t);
+	merge(t, t, R);
+
+	return ans;
+}
+
+void print(treap t){
+	if(!t) return;
+	print(t->l);
+	// printf("(%d,%d,%d) ", t->key, t->cnt, t->mindif);
+		cout << '(' << t->key << "," << t->cnt << "," << t->mn << "," << t->mx << "," << t->mindif << ") ";
+	print(t->r);
+}
+
+
+int main(void){
+	// ios_base::sync_with_stdio(false);
+	int n;
+	// cin >> n;
+	scanf("%d", &n);
+
+	treap t = NULL;
+
+	char op;
+	int k, l, r;
+	while(n--){
+		// cin >> op;
+		scanf(" %c", &op);
+		int ans;
+
+		switch(op){
+			case 'I':
+				// cin >> k;
+				scanf("%d", &k);
+				insert(t, new item(k));
+				break;
+			case 'D':
+				// cin >> k;
+				scanf("%d", &k);
+				erase(t, k);
+				break;
+			case 'N':
+				// cin >> l >> r;
+				scanf("%d%d", &l, &r);
+				ans = find_mindif(t, l+1, r+1);
+				printf("%d\n", ans);
+				break;
+			case 'X':
+				// cin >> l >> r;
+				scanf("%d%d", &l, &r);
+				if(l < r){
+					ans = find_kth(t, r+1) - find_kth(t, l+1);
+					printf("%d\n", ans);
+				}
+				else
+					printf("-1\n");
+				break;
+			// case 'P':
+			// 	print(t);
+			// 	// cout << endl;
+			// 	printf("\n");
+			// 	break;
+		}
+	}
+
+	return 0;
+}
